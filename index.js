cf42784317ce04a0b10bcdfaad6b9a98102009e5
fmussap <fabiomussap@yahoo.com>
2017-11-07 10:06:27 +0000

    update 07/11

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const express = require('express')
+require('./services/passport')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there!!!' })
-})
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
+
 app.listen(PORT)
